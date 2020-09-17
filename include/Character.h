c2fc97663675c012711714519435d3f72e4430af
twct <timothy@thrift.dev>
2020-09-17 15:38:21 -0500

    Character: Add functions for controlling character objects

--- include/Character.h
+++ include/Character.h
@@ -4,14 +4,29 @@
 #include <Sprite.h>
 #include <World.h>
 
+enum Direction
+{
+    DIRECTION_NONE,
+    DIRECTION_RIGHT,
+    DIRECTION_LEFT
+};
+
 class Character
 {
 private:
     CtxPtr m_context;
     Sprite m_sprite;
+    Vector2f m_velocity;
+    double m_moveSpeed, m_fallSpeed, m_fric;
+    Direction m_direction;
 public:
     Character(const CtxPtr &context, const Atlas &atlas);
+    void direction(const Direction &direction);
+    void moveSpeed(const double moveSpeed);
+    void fallSpeed(const double fallSpeed);
     Sprite &sprite();
+    void draw();
+    void update();
 };
 
 #endif
--- src/Character.cpp
+++ src/Character.cpp
@@ -1,14 +1,71 @@
 #include <Character.h>
+#include <Log.h>
 
 Character::Character(const CtxPtr &context, const Atlas &atlas) :
     m_context(context)
 {
+    moveSpeed(0.2);
+    fallSpeed(0.05);
+
+    m_fric = 1.1;
+    m_direction = DIRECTION_NONE;
+
     m_sprite.texture(m_context->assetLoader->getTexture(atlas.textures.at(atlas.id).id));
     m_sprite.atlas(atlas);
     m_sprite.playAnimation(atlas.defaultAnimation);
 }
 
+void Character::direction(const Direction &direction)
+{
+    m_direction = direction;
+}
+
+void Character::moveSpeed(const double moveSpeed)
+{
+    m_moveSpeed = moveSpeed;
+}
+
+void Character::fallSpeed(const double fallSpeed)
+{
+    m_fallSpeed = fallSpeed;
+}
+
 Sprite &Character::sprite()
 {
     return m_sprite;
+}
+
+void Character::draw()
+{
+    m_sprite.draw(m_context->renderer);
+}
+
+void Character::update()
+{
+    auto position = m_sprite.position();
+
+    switch (m_direction) {
+        case DIRECTION_LEFT:
+            m_velocity.x -= m_moveSpeed;
+            m_sprite.playAnimation("run");
+            m_sprite.fliph();
+        break;
+        case DIRECTION_RIGHT:
+            m_velocity.x += m_moveSpeed;
+            m_sprite.playAnimation("run");
+            m_sprite.flipn();
+        break;
+        case DIRECTION_NONE:
+            m_sprite.playAnimation("idle");
+        break;
+    }
+
+
+    m_velocity.x /= m_fric;
+    m_velocity.y += m_fallSpeed;
+
+    position.x += m_velocity.x;
+    position.y += m_velocity.y;
+
+    m_sprite.position(position);
 }
